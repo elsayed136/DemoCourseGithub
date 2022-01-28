40469071606c3b3a234080e5910ed143fd992f21
abdulhameedkhaled <abdulhameed.khaled12@gmail.com>
2022-01-28 21:38:49 +0200

    hello

--- DemoMVC/Controllers/helloController.cs
+++ DemoMVC/Controllers/helloController.cs
@@ -3,7 +3,7 @@
     public class helloController 
     {
         public int AasmhasAnEWtAST { get; set; }
-        public int YousefisABadleader { get; set; }
+       
         public int AzizIsMyFreidnd { get; set; }
 
         public int SayedIsA3l2 { get; set; }
